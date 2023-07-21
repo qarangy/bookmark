9ed818641a8e3c6491ee17a2cbd340f1fdc4796c
qarangy <qarangy@gmail.com>
2023-07-21 10:26:46 +0500

    add FAQ, Accordion sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
 const elsTabsItem = document.querySelectorAll('.tabs__item');
 const elsTabLink = document.querySelectorAll('.js-tabs-link');
+const elsAccordionItem = document.querySelectorAll('.accordion__item');
+const elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
 
 function deactivateTabItems () {
   elsTabsItem.forEach(function (elTabsItem) {
@@ -11,6 +13,12 @@ function deactivateTabItems () {
 function deactivateTabPanels () {
   elsTabsPanel.forEach(function (elTabsPanel) {
     elTabsPanel.classList.remove('tabpanels__item--active');
+  });
+}
+
+function closeAccordionItem () {
+  elsAccordionItem.forEach(function (elAccordionItem) {
+    elAccordionItem.classList.remove('accordion__item--open');
   });
 }
 
@@ -28,4 +36,12 @@ elsTabLink.forEach (function (elTabLink) {
 
     elTargetPanel.classList.add('tabpanels__item--active');
   });
+});
+
+elsAccordionItemToggler.forEach (function (elAccordionItemToggler) {
+  elAccordionItemToggler.addEventListener('click', function () {
+
+    closeAccordionItem();
+    elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
+  })
 });
